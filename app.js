973be061a3610a6944e58afd1073df7f72021225
Steven Taylor <steven.taylor@me.com>
2018-10-14 13:34:12 -0700

    added newrelic

--- app.js
+++ app.js
@@ -54,6 +54,10 @@ consul.kv.get(`config/sentinel/${moduleName}`, function(err, result) {
     global.config = config;
     global.config.save();
 
+    if (!global.config.newrelic) {
+        require('newrelic');
+    }
+
     let pub = redis.createClient(
         {
             host: process.env.REDIS || global.config.redis || '127.0.0.1',
